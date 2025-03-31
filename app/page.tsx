3d66a842a64bc2818bea5bdd35d3a31cfbd4695a
coolkid!playcode <161105950+CamilaTran@users.noreply.github.com>
2025-03-31 14:39:12 +0700

    change size

--- app/page.tsx
+++ app/page.tsx
@@ -18,13 +18,13 @@ export default function Home() {
   }, [])
 
   return (
-    <div className="xl:max-w-[1216px] sm:max-w-[704px] !mx-auto xl:!py-24 lg:!py-16 !py-12 !px-4">
+    <div className="lg:max-w-[1216px] sm:max-w-[704px] !mx-auto lg:!py-24 sm:!py-16 !py-12 !px-4">
       <div className="flex flex-col gap-8">
         <div className="flex justify-between items-center">
           <h3 className="font-semibold text-3xl">Latest Arrivals</h3>
           <button className="border border-[0.5px] border-neutral-200 rounded-sm !py-[10px] !px-[18px] shadow font-medium">View all</button>
         </div>
-        <div className="grid xl:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-8 justify-center">
+        <div className="grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-8 justify-center">
           {data?.map((item: Product) => <ProductCard info={item} key={item.product_id} />)}
         </div>
       </div>
